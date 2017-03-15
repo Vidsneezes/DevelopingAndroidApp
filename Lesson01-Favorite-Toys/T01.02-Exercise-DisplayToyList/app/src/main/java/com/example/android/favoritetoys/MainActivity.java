503cec9f9a23fdb489d5cde2721fc181e1fd5c3c
Vidsneezes <moraleszez@gmail.com>
2017-03-15 10:49:17 -0600

    got mToysListTextView via findViewById

--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -21,7 +21,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Declare a TextView variable called mToysListTextView
     TextView mToysListTextView;
 
     @Override
@@ -29,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (3) Use findViewById to get a reference to the TextView from the layout
+        mToysListTextView = (TextView)findViewById(R.id.tv_toy_names);
         // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
 
         // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
