ceef4f18813aa3b617db6d2464bb068bf2e89d45
Vidsneezes <moraleszez@gmail.com>
2017-04-14 08:24:32 -0600

    removed toast message, add call to makeGithubSearchQuery in onOptionsItemSelected

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
     void makeGirhubSearchQuery(){
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL url = NetworkUtils.buildUrl(githubQuery);
-
         mUrlDisplayTextView.setText(url.toString());
     }
     // TODO (3) Within this method, build the URL with the text from the EditText and set the built URL to the TextView
@@ -67,10 +66,8 @@ public class MainActivity extends AppCompatActivity {
         int itemThatWasClickedId = item.getItemId();
         if (itemThatWasClickedId == R.id.action_search) {
             // TODO (4) Remove the Toast message when the search menu item is clicked
-            Context context = MainActivity.this;
-            String textToShow = "Search clicked";
-            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
             // TODO (5) Call makeGithubSearchQuery when the search menu item is clicked
+            makeGirhubSearchQuery();
             return true;
         }
         return super.onOptionsItemSelected(item);
